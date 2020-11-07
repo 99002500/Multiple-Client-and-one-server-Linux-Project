f6c97b424942c2b4588e0f97e24eec9fe39467bd
99002500 <sampreeth.rayadurga@ltts.com>
2020-11-07 15:27:56 +0530

    started mainpart

--- chatroom.c
+++ chatroom.c
@@ -13,3 +13,25 @@
 #define MAX_CLIENTS 100
 #define BUFFER_SZ 2048
 
+int main(int argc, char **argv)
+{
+    if(argc !=2)
+    {
+        printf("Usage %s <port>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    char *ip = "127.0.0.1";
+    int port = atoi(argv[1]);
+    int option=1;
+    int listenfd=0, connfd=0;
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in cli_addr;
+    pthread_t tid;
+
+    // Now we have to do settings for socket
+ listenfd = socket(AF_INET, SOCK_STREAM, 0);
+ serv_addr.sin_family = AF_INET;
+ serv_addr.sin_addr
+
+    
+}
