32aa12fb4d6962813e32be75612c7782b51051ac
99002500 <sampreeth.rayadurga@ltts.com>
2020-11-07 14:54:32 +0530

    included header files

---
+++ chatroom.c
@@ -0,0 +1,15 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <signal.h>
+
+#define MAX_CLIENTS 100
+#define BUFFER_SZ 2048
+
