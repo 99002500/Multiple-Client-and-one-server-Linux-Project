53fc8d5ca574c65b69e8441519f918b7ab879932
99002500 <sampreeth.rayadurga@ltts.com>
2020-11-07 15:37:18 +0530

    binding and listening done

--- chatroom.c
+++ chatroom.c
@@ -13,6 +13,10 @@
 #define MAX_CLIENTS 100
 #define BUFFER_SZ 2048
 
+static _Atomic unsigned int cli_count = 0;
+static int uid = 10;
+
+
 int main(int argc, char **argv)
 {
     if(argc !=2)
@@ -34,7 +38,28 @@ int main(int argc, char **argv)
  serv_addr.sin_addr.s_addr = inet_addr(ip);
  serv_addr.sin_port = htons(port);
 
- 
+ // Now we do for signals which are basically system generated interrupts
 
+ signal(SIGPIPE, SIG_IGN);
+ if(setsockopt(listenfd, SOL_SOCKET, (SO_REUSEPORT | SO_REUSEADDR), (char*)&option, sizeof(option))<0)
+{
+    printf("ERROR: setsocketopt\n");
+    return EXIT_FAILURE;
+}
+// Now we are going to bind the address
+
+if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))<0)
+{
+ printf("ERROR: listen\n");   
+}
     
+ // listen
+ if(listen(listenfd, 10)<0)
+ {
+     printf("Error: listen\n");
+     return EXIT_FAILURE;
+ }
+
+
+
 }
