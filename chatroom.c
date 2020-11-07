bdd02ef5d51195cbf6e2d7224592cd8afa592af3
99002500 <sampreeth.rayadurga@ltts.com>
2020-11-07 15:59:19 +0530

    add clients to the queue

--- chatroom.c
+++ chatroom.c
@@ -16,15 +16,53 @@
 static _Atomic unsigned int cli_count = 0;
 static int uid = 10;
 
-/* Client structure */
+// Client structure
 typedef struct{
-	struct sockaddr_in address;
-	int sockfd;
-	int uid;
-	char name[32];
+struct sockaddr_in address;
+int sockfd;
+int uid;
+char name[32];
 } client_t;
 
+client_t *clients[MAX_CLIENTS];
 
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void str_overwrite_stdout() {
+    printf("\r%s", "> ");
+    fflush(stdout);
+}
+
+void str_trim_lf (char* arr, int length) {
+  int i;
+  for (i = 0; i < length; i++) { // trim \n
+    if (arr[i] == '\n') {
+      arr[i] = '\0';
+      break;
+    }
+  }
+}
+
+void print_client_addr(struct sockaddr_in addr){
+    printf("%d.%d.%d.%d",
+        addr.sin_addr.s_addr & 0xff,
+        (addr.sin_addr.s_addr & 0xff00) >> 8,
+        (addr.sin_addr.s_addr & 0xff0000) >> 16,
+        (addr.sin_addr.s_addr & 0xff000000) >> 24);
+}
+ // Now we have to add clients to the queue
+void queue_add(client_t *cl){
+	pthread_mutex_lock(&clients_mutex);
+
+	for(int i=0; i < MAX_CLIENTS; ++i){
+		if(!clients[i]){
+			clients[i] = cl;
+			break;
+		}
+	}
+
+	pthread_mutex_unlock(&clients_mutex);
+}
 
 
 
